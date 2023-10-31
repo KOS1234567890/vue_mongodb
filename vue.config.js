f1b06a3ba2e2bd03b1cbbf49fd90104a6203d35d
KOS1234567890 <fistar02@gmail.com>
2023-10-31 09:55:59 +0900

    a

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // publicPath:'vue_project1' 깃페이지
 })
